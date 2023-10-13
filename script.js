41ea11843693538fcf088791c1216fae3d168c6f
Orxan-Abdullazada <tu7pnc7lt@code.edu.az>
2023-10-13 12:48:52 +0400

    Array methods

---
+++ script.js
@@ -0,0 +1,107 @@
+// for (let i = 5; i >= 1; i--) {
+//    let k=''
+//    for (let j = 0; j < i; j++) {
+//   k+='*'
+    
+//    }
+//     console.log(k);
+// }
+// push pop shift unshift
+// sort 
+//  let arr=[7,2,4,16,11,1]
+//  let yaddas 
+ 
+//  for (let i = 0; i < arr.length ; i++) {
+
+//     for (let j = i+1; j < arr.length; j++) {
+        
+//         if (arr[i]>arr[j]) {
+// yaddas=arr[j]
+// arr[j]=arr[i]
+// arr[i]=yaddas            
+//         }
+//     }
+   
+//  }
+//  console.log(arr);
+
+
+
+// let arr=[7,2,4,16,11,1]
+// let yaddas 
+
+// for (let i = 0; i < arr.length ; i++) {
+
+//    for (let j = i+1; j < arr.length; j++) {
+       
+//        if (arr[i]>arr[j]) {
+        
+// yaddas=arr[j]
+// arr[j]=arr[i]
+// arr[i]=yaddas    
+
+//    console.log(arr); 
+           
+// }
+//    }
+  
+// }
+
+// let arr=[2,4,5,6,7,8,9,3]
+// arr.forEach(element => {
+//     if (element%2===0) {
+//       console.log(element);
+
+//     }
+// }); 
+// 3333333333333333333333333333333333
+// const arr=[2,4,1,3,5,1,7,9]
+// const arrr=arr.slice(0,4)
+// console.log(arrr);
+// 4444444444444444444444444444444
+// const arr1=[1,2,3]
+// const arr2=[4,5,6]
+// const arr3=arr1.concat(arr2)
+// console.log(arr3);
+// 5555555555555555555555555555555555
+// const arr=[1,2,3,4,5,6,6,1,8,1,4,1]
+// const result=arr.filter(chekOne)
+// function chekOne(one) {
+//     return one===1
+// }
+// console.log(result);
+// 6666666666666666666666666666666666666
+// const arr=[1,2,3,4,5,6,6,1,8,1,4,1]
+// const result=arr.filter(chekOne)
+// function chekOne(one) {
+//     return one!==1
+// }
+// console.log(result);
+// 7777777777777777777777777
+// const arr=["salam",1,2]
+// arr.unshift("Nura")
+// console.log(arr);
+// 88888888888888888888888888
+// const arr=["salam",1,2]
+// arr.push("Yellow")
+// console.log(arr);
+// 99999999999999999999999999999999
+// const arr=["salam",1,2]
+// arr.shift("Nura")
+// console.log(arr);
+// 10101010101010101
+// const arr=["salam",1,2]
+// arr.pop()
+// console.log(arr);
+// 11111111111111111111111111111111111
+// const arr=["salam",1,2]
+// arr.splice(1,1)
+// console.log(arr);
+// 12121212212121211221212121221
+// const arr1=[1,2,3]
+// const arr2=[... arr1]
+// arr1.pop()
+// console.log(arr1);
+// console.log(arr2);
+
+
